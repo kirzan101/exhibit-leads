53dfca9d6ac2562d15b3dc5c965aa9d49dfeca7a
christian <ckimescamilla@gmail.com>
2023-07-17 15:49:29 +0800

    added web favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
     @inertiaHead
