55e50847a036b1edefab235f4fc60a9b50141dfb
christian <ckimescamilla@gmail.com>
2023-08-03 10:26:39 +0800

    remove test page

--- routes/web.php
+++ routes/web.php
@@ -85,7 +85,6 @@ Route::middleware('auth')->group(function () {
 
     //sources
     Route::resource('/sources', SourceController::class);
-    Route::get('/test', [SourceController::class, 'testDropDown']);
 
     Route::get('/usergroups', [UserGroupController::class, 'index']);
 });
