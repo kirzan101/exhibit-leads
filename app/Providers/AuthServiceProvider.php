bbfc475dcc5675cbad83ef0664fef4fcde91dd2e
christian <ckimescamilla@gmail.com>
2023-09-22 18:59:36 +0800

    register policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,25 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 
-use App\Policies\MemberPolicy;
+use App\Models\AssignedConfirmer;
+use App\Models\AssignedEmployee;
+use App\Models\AssignedExhibitor;
+use App\Models\Employee;
+use App\Models\Lead;
+use App\Models\UserGroup;
+use App\Models\Venue;
+use App\Policies\AssignedConfirmerPolicy;
+use App\Policies\AssignedEmployeePolicy;
+use App\Policies\AssignedExhibitorPolicy;
+use App\Policies\EmployeePolicy;
+use App\Policies\ExhibitPolicy;
+use App\Policies\LeadPolicy;
+use App\Policies\RoiPolicy;
+use App\Policies\SourcePolicy;
+use App\Policies\StatusPolicy;
+use App\Policies\SurveyPolicy;
+use App\Policies\UserGroupPolicy;
+use App\Policies\VenuePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -15,7 +33,18 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        Member::class => MemberPolicy::class,
+        AssignedConfirmer::class => AssignedConfirmerPolicy::class,
+        AssignedEmployee::class => AssignedEmployeePolicy::class,
+        AssignedExhibitor::class => AssignedExhibitorPolicy::class,
+        Employee::class => EmployeePolicy::class,
+        Exhibit::class => ExhibitPolicy::class,
+        Lead::class => LeadPolicy::class,
+        Roi::class => RoiPolicy::class,
+        Source::class => SourcePolicy::class,
+        Status::class => StatusPolicy::class,
+        Survey::class => SurveyPolicy::class,
+        UserGroup::class => UserGroupPolicy::class,
+        Venue::class => VenuePolicy::class,
     ];
 
     /**
