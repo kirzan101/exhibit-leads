1541c9fe09af511e5434512dae819f2c19467994
christian <ckimescamilla@gmail.com>
2023-10-20 18:46:07 +0800

    change confirmer venue selection to one venue only

--- resources/js/Components/Modals/ConfirmerVenueModal.vue
+++ resources/js/Components/Modals/ConfirmerVenueModal.vue
@@ -10,10 +10,10 @@
         <div class="d-block">
             <h5>Please choose venues:</h5>
             <b-form-group
-                label="Can select multiple:"
+                label=""
                 v-slot="{ ariaDescribedby }"
             >
-                <b-form-checkbox-group
+                <!-- <b-form-checkbox-group
                     id="checkbox-group-1"
                     :options="
                         venue_list.map((item) => {
@@ -26,13 +26,43 @@
                     :aria-describedby="ariaDescribedby"
                     name="veunue-list"
                     v-model="current_venues"
-                ></b-form-checkbox-group>
+                ></b-form-checkbox-group> -->
+
+                <b-form-radio-group
+                    id="radio-slots"
+                    v-model="current_venues"
+                    :options="
+                        venue_list.map((item) => {
+                            return {
+                                text: item.name,
+                                value: item.id,
+                            };
+                        })
+                    "
+                    :aria-describedby="ariaDescribedby"
+                    name="radio-options-slots"
+                ></b-form-radio-group>
             </b-form-group>
         </div>
 
         <template #modal-footer>
-            <b-button v-if="current_venues.length > 0" variant="info" @click="submit()"> Confirm </b-button>
-            <b-button v-else variant="info" :disabled="true"> Confirm </b-button>
+            <!-- <b-button
+                v-if="current_venues.length > 0"
+                variant="info"
+                @click="submit()"
+            >
+                Confirm
+            </b-button> -->
+            <b-button
+                v-if="current_venues > 0"
+                variant="info"
+                @click="submit()"
+            >
+                Confirm
+            </b-button>
+            <b-button v-else variant="info" :disabled="true">
+                Confirm
+            </b-button>
         </template>
     </b-modal>
 </template>
@@ -48,13 +78,25 @@ export default {
     },
     data() {
         return {
-            current_venues: [],
+            // current_venues: [],
+            current_venues: "",
         };
     },
     methods: {
         submit() {
+            // const form = {
+            //     venue_ids: this.current_venues,
+            //     employee_id: this.employee_id,
+            // };
+
+            //for radio button
+            // remove if requested is multiple
+            const venue_id = [];
+            venue_id.push(this.current_venues); // convert to array
+
             const form = {
-                venue_ids: this.current_venues,
+                // venue_ids: this.current_venues, //for checkbox
+                venue_ids: venue_id,
                 employee_id: this.employee_id,
             };
 
@@ -62,25 +104,24 @@ export default {
                 _method: "PUT",
                 ...form,
             });
-
         },
         showConfirmerVenueModal() {
             this.$refs["confirmer-venue-modal"].show();
         },
     },
-    watch: {
-        current_venues() {
-            if (this.employee_venue_list.length > 0) {
-                return (this.current_venues = employee_venue_list.map(
-                    (item) => {
-                        return item.venue_id;
-                    }
-                ));
-            }
+    // watch: {
+    //     current_venues() {
+    //         if (this.employee_venue_list.length > 0) {
+    //             return (this.current_venues = employee_venue_list.map(
+    //                 (item) => {
+    //                     return item.venue_id;
+    //                 }
+    //             ));
+    //         }
 
-            return [];
-        },
-    },
+    //         return [];
+    //     },
+    // },
     mounted() {
         this.showConfirmerVenueModal();
     },
