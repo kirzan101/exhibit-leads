47f94a35eef89562701c7e8c93df54b66d269c3f
christian <ckimescamilla@gmail.com>
2024-02-26 19:30:39 +0800

    added mobile login

--- app/Http/Controllers/API/OpcLeadController.php
+++ app/Http/Controllers/API/OpcLeadController.php
@@ -7,15 +7,18 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\OpcLeadResource;
 use App\Models\OpcLead;
+use App\Models\User;
 use App\Services\OpcLeadService;
 use Exception;
 use Illuminate\Support\Facades\Auth;
+use App\Services\AuthMobileService;
 
 class OpcLeadController extends Controller
 {
-    public function __construct(private OpcLeadService $opcLeadService)
+    public function __construct(private OpcLeadService $opcLeadService, private AuthMobileService $authMobileService)
     {
         $this->opcLeadService = $opcLeadService;
+        $this->authMobileService = $authMobileService;
     }
 
     /**
@@ -101,5 +104,79 @@ class OpcLeadController extends Controller
         return response()->json([
             'message' => $message,
         ], $response_code);
+    }
+
+    /**
+     * login the user
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function loginMobile(Request $request)
+    {
+        //decrypt password
+        $request->merge(['password' => $this->authMobileService->decryptMobilePassword($request->password)]);
+
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+
+            // if account is deactivated
+            if (!Auth::user()->is_active) {
+
+                Auth::logout();
+
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => [
+                        'email' => 'The account is deactivated.'
+                    ]
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged In Successfully',
+                'user' => $user,
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
+        }
+
+        return response()->json([
+            'status' => false,
+            'message' => 'The provided credentials do not match our records.',
+        ], 401);
+    }
+
+    /**
+     * Encrypt password
+     *
+     * @param Request $password
+     * @return void
+     */
+    public function encryptMobilePassword(Request $request)
+    {
+        $encrypted_password = $this->authMobileService->encryptMobilePassword($request->password);
+
+        return $encrypted_password;
+    }
+
+    /**
+     * Decrypt password
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function decryptMobilePassword(Request $request)
+    {
+        $decrypted_password = $this->authMobileService->decryptMobilePassword($request->password);
+
+        return $decrypted_password;
     }
 }
